71718266609ff650e5fdffa60c9a550b59cbddde
Alix Heudebourg <alix.heudebourg@gmail.com>
2025-08-13 21:18:40 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+app.get('/plants', (req, res) => {
+  res.json([
+    { name: 'Carrot', emoji: '🥕' },
+    { name: 'Zucchini', emoji: '🥒' }
+  ]);
+});
+
+app.listen(8000, () => {
+  console.log('Backend running on http://localhost:8000');
+});
+
+/* Endpoints i need:
+
+- get my biome
+- get my current plants (my garden)
+- add a plant to my garden
+- remove a plant from my garden
+- edit crop in my garden (harvest, grow, etc.)
+
+
+- get all biomes
+- get all available plants in my area/biome
+*/
